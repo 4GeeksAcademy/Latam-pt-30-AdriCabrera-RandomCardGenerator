1c6027e9c796b0686485239edebe8a7b1e38d52a
Adriana Cabrera Escobar <160674673+AdriCabrera@users.noreply.github.com>
2024-04-25 01:56:53 +0000

    Random Card Generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const randomSuit = generateRandomSuit();
+  if (randomSuit === "♦" || randomSuit === "♥") {
+    document.querySelector("#top-suit").style.color = "red";
+    document.querySelector("#bottom-suit").style.color = "red";
+  }
+  document.querySelector("#top-suit").innerHTML = randomSuit;
+  document.querySelector("#number").innerHTML = generateRandomNumber();
+  document.querySelector("#bottom-suit").innerHTML = randomSuit;
+};
+
+const generateRandomNumber = () => {
+  const numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  return randomNumber;
+};
+
+const generateRandomSuit = () => {
+  const suits = ["♦", "♥", "♠", "♣"];
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  return randomSuit;
 };
